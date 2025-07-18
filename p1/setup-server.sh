a951ab1e97d727ca22892e0f599c2027453d7685
KGebski0036 <kgebski68@gmail.com>
2025-07-18 18:55:47 +0200

    disable fire wall

--- p1/setup-server.sh
+++ p1/setup-server.sh
@@ -2,15 +2,26 @@
 
 # Install dependencies
 sudo apt-get update
-sudo apt-get install -y curl
+sudo apt-get install -y curl net-tools
+
+sudo systemctl disable firewalld --now
+sudo setenforce 0
+sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+
+sudo uwf disable
+sudo kill $( lsof -i:6443 -t )
+sudo /usr/local/bin/k3s-uninstall.sh
 
 # Install K3s in server mode
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
+curl -sfL https://get.k3s.io | INSTLL_K3S_EXEC="server --node-ip 192.168.56.110 --write-kubeconfig-mode 0644 --token 12345" sh -s -
 
+echo 'alias k="kubectl"' >> /home/vagrant/.bashrc
+sudo chmod 777 /etc/rancher/k3s/k3s.yaml
+source /home/vagrant/.bashrc
 # Copy kubeconfig to vagrant user
-mkdir -p /home/vagrant/.kube
-sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-sudo chown -R vagrant:vagrant /home/vagrant/.kube
+# mkdir -p /home/vagrant/.kube
+# sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+# sudo chown -R vagrant:vagrant /home/vagrant/.kube
 
 # Save join token to shared folder
-sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
+# sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
--- p1/setup-worker.sh
+++ p1/setup-worker.sh
@@ -2,10 +2,15 @@
 
 # Install dependencies
 sudo apt-get update
-sudo apt-get install -y curl
+sudo apt-get install -y curl net-tools
 
+sudo systemctl disable firewalld --now
+sudo setenforce 0
+sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+sudo /usr/local/bin/k3s-uninstall.sh
 # Read token from shared folder
-TOKEN=$(cat /vagrant/node-token)
+export TKN=$(cat /vagrant/token)
 
 # Install K3s in agent mode
-curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="$TOKEN" sh -
+curl -sfL https://get.k3s.io | INSTLL_K3S_EXEC="agent --server https://192.168.56.110:6443 --node-ip 192.168.56.110 --token 12345" sh -s -
+echo "Done"
