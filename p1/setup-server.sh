09de78bf37f6c3120aed8adf20ea6bf1f41ed30a
KGebski0036 <kgebski68@gmail.com>
2025-07-18 19:05:50 +0200

    disable firewall

--- p1/setup-server.sh
+++ p1/setup-server.sh
@@ -4,24 +4,25 @@
 sudo apt-get update
 sudo apt-get install -y curl net-tools
 
-sudo systemctl disable firewalld --now
-sudo setenforce 0
-sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+sudo iptables -F
+sudo iptables -X
+sudo iptables -t nat -F
+sudo iptables -t nat -X
 
-sudo uwf disable
-sudo kill $( lsof -i:6443 -t )
-sudo /usr/local/bin/k3s-uninstall.sh
+if [ -f /usr/local/bin/k3s-uninstall.sh ]; then
+    sudo /usr/local/bin/k3s-uninstall.sh
+fi
 
 # Install K3s in server mode
-curl -sfL https://get.k3s.io | INSTLL_K3S_EXEC="server --node-ip 192.168.56.110 --write-kubeconfig-mode 0644 --token 12345" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip 192.168.56.110 --write-kubeconfig-mode 0644 --token 12345" sh -s -
+
+mkdir -p /home/vagrant/.kube
+sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+sudo chown -R vagrant:vagrant /home/vagrant/.kube
+sudo chmod 644 /home/vagrant/.kube/config
 
 echo 'alias k="kubectl"' >> /home/vagrant/.bashrc
-sudo chmod 777 /etc/rancher/k3s/k3s.yaml
-source /home/vagrant/.bashrc
-# Copy kubeconfig to vagrant user
-# mkdir -p /home/vagrant/.kube
-# sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-# sudo chown -R vagrant:vagrant /home/vagrant/.kube
 
-# Save join token to shared folder
-# sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
+source /home/vagrant/.bashrc
+
+echo "K3s server setup complete."
--- p1/setup-worker.sh
+++ p1/setup-worker.sh
@@ -4,13 +4,17 @@
 sudo apt-get update
 sudo apt-get install -y curl net-tools
 
-sudo systemctl disable firewalld --now
-sudo setenforce 0
-sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
-sudo /usr/local/bin/k3s-uninstall.sh
-# Read token from shared folder
-export TKN=$(cat /vagrant/token)
+sudo iptables -F
+sudo iptables -X
+sudo iptables -t nat -F
+sudo iptables -t nat -X
+
+# Remove K3s if already installed
+if [ -f /usr/local/bin/k3s-uninstall.sh ]; then
+    sudo /usr/local/bin/k3s-uninstall.sh
+fi
 
 # Install K3s in agent mode
-curl -sfL https://get.k3s.io | INSTLL_K3S_EXEC="agent --server https://192.168.56.110:6443 --node-ip 192.168.56.110 --token 12345" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --node-ip 192.168.56.110 --token 12345" sh -s -
+
 echo "Done"
