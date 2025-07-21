cd3d7bbc4c9790117531e4e83cfc4908388e3a23
Piotr Biedermann <pbiederm@student.42warsaw.pl>
2025-07-21 19:34:37 +0200

    p1 works now with alpine318

--- p1/setup-server.sh
+++ p1/setup-server.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
-
-# Install dependencies
-sudo apt-get update
-sudo apt-get install -y curl net-tools
-
-sudo iptables -F
-sudo iptables -X
-sudo iptables -t nat -F
-sudo iptables -t nat -X
-
-if [ -f /usr/local/bin/k3s-uninstall.sh ]; then
-    sudo /usr/local/bin/k3s-uninstall.sh
-fi
-
-# Install K3s in server mode
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip 192.168.56.110 --write-kubeconfig-mode 0644 --token 12345" sh -s -
-
-mkdir -p /home/vagrant/.kube
-sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-sudo chown -R vagrant:vagrant /home/vagrant/.kube
-sudo chmod 644 /home/vagrant/.kube/config
-
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -
+NODE_TOKEN="/var/lib/rancher/k3s/server/node-token"
+while [ ! -f ${NODE_TOKEN} ]
+do
+     sleep 1
+done
+sudo cp ${NODE_TOKEN} /vagrant/
 echo 'alias k="kubectl"' >> /home/vagrant/.bashrc
-
-source /home/vagrant/.bashrc
-
-echo "K3s server setup complete."
--- p1/setup-worker.sh
+++ p1/setup-worker.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 
-# Install dependencies
-sudo apt-get update
-sudo apt-get install -y curl net-tools
-
-sudo iptables -F
-sudo iptables -X
-sudo iptables -t nat -F
-sudo iptables -t nat -X
-
-# Remove K3s if already installed
-if [ -f /usr/local/bin/k3s-uninstall.sh ]; then
-    sudo /usr/local/bin/k3s-uninstall.sh
-fi
-
-# Install K3s in agent mode
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --node-ip 192.168.56.110 --token 12345" sh -s -
-
-echo "Done"
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(sudo cat /vagrant/node-token) sh -
