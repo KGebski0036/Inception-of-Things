3b892677db5e563d04700e7e6f07cbed0138a3cb
Kgebski0036 <kgebski68@gmail.com>
2025-07-22 15:48:36 +0000

    finish p2

--- p2/setup-server.sh
+++ p2/setup-server.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -
+
+until kubectl get nodes &>/dev/null; do
+    sleep 2
+done
+
+cd /vagrant
+
+kubectl apply -f app1.yaml --validate=false
+kubectl apply -f app2.yaml --validate=false
+kubectl apply -f app3.yaml --validate=false
+kubectl apply -f ingress.yaml --validate=false
